3c69c03bcbbaf5b6d5b087108f3049d2c484d914
Comgen21 <comgendev21@gmail.com>
2021-09-03 12:48:20 +0530

    first commit

---
+++ app/src/main/java/com/comgen/poc/toastlib/MainActivity.java
@@ -0,0 +1,15 @@
+package com.comgen.poc.toastlib;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toaster.customToast(this,"Hey am from your custom lib");
+    }
+}
